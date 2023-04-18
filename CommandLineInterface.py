247ca72cd71113aaf4c6174b61e31a6b275c0f8d
Michael Albert <buzz66boy@users.noreply.github.com>
2023-04-18 12:00:03 -0600

    CommandLineInterface base class and attributes, method stubs

---
+++ CommandLineInterface.py
@@ -0,0 +1,19 @@
+class CommandLineInterface:
+    
+    def __init__(self):
+        self.curScreen = "start"
+        self.history = []
+
+    def __help():
+        return ""
+
+    def submitText(string):
+        return string
+
+    def startSession():
+        pass
+
+    def display(string):
+        pass
+
+    
