dc89d0547ab1adbbc8a44ae9856d36f9c6c22698
vic12141983 <60449014+vic12141983@users.noreply.github.com>
2023-04-22 15:14:10 -0600

    User Class with methods

--- User.py
+++ User.py
@@ -28,3 +28,8 @@ class User:
         self.administrator = None
         self.payment_method = None
         self.bills = None
+
+
+# Test
+user1 = User("Victor", "verification", False, "Fed Now", 50.0)
+print(f" User name: {user1.username},\n Current Password: {user1.password}")
