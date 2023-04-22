92e3089b029e51987de0ae4c9a3609aaef12d50e
vic12141983 <60449014+vic12141983@users.noreply.github.com>
2023-04-22 15:04:16 -0600

    User Class with methods

---
+++ User.py
@@ -0,0 +1,33 @@
+class User:
+    def __init__(self, username, password, administrator, payment_method, bills):
+        self.username = username
+        self.password = password
+        self.administrator = administrator
+        self.payment_method = payment_method
+        self.bills = bills
+
+    def create_account(self, username, password):
+        self.username = username
+        self.password = password
+
+    def modify_account(self, username=None, password=None, administrator=None, payment_method=None, bills=None):
+        if username:
+            self.username = username
+        if password:
+            self.password = password
+        if administrator is not None:
+            self.administrator = administrator
+        if payment_method:
+            self.payment_method = payment_method
+        if bills:
+            self.bills = bills
+
+    def delete_account(self):
+        self.username = None
+        self.password = None
+        self.administrator = None
+        self.payment_method = None
+        self.bills = None
+user1 = User("jdoe", "password123", False, "credit_card", 50.0)
+user1.modify_account(bills=75.0)
+print(user1.bills)
