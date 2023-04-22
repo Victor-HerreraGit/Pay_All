34a27893e87c457ea9d6ea0d811d2d25188f1078
cakleffman <cakleffman@gmail.com>
2023-04-21 23:14:06 -0600

    Billing Base Class v2

--- Bill.py
+++ Bill.py
@@ -4,3 +4,24 @@ class Bill:
         self.payment_method = payment_method
         self.billing_accounts = []
         self.payment_history = []
+
+    def view_bills(self):
+        pass
+
+    def setUpRecurringPayments(self):
+        pass
+
+    def linkBillingAccount(self):
+        pass
+
+    def deleteBillingAccount(self):
+        pass
+
+    def addBillingAccount(self):
+        pass
+
+    def pay_bill(self):
+        pass
+
+    def viewPaymentHistory(self):
+        pass
